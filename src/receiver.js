251aa28458ef5333a6432850ab44321cf270ff72
Stephan-MDD <stephan.mdd@gmail.com>
2020-11-05 13:35:27 +0100

    added stuff

--- src/receiver.js
+++ src/receiver.js
@@ -1,27 +1,67 @@
-const amqp = require("amqplib/callback_api");
+// libraries
+const amqp = require('amqplib/channel_api');
+const fetch = require('node-fetch');
+const eip = require('eip');
+const fs = require('fs');
 
-//Step 1: Create connection
-amqp.connect("amqp://localhost", (connError, connection) => {
-  if (connError) {
-    throw connError;
-  }
-  //Step 2: Create channel
-  connection.createChannel((channelError, channel) => {
-    if (channelError) {
-      throw channelError;
-    }
-    //Step 3: Assert queue
-    const QUEUE = "codingtest";
-    channel.assertQueue(QUEUE);
-    //Step 4: Receive messages
-    channel.consume(
-      QUEUE,
-      (msg) => {
-        console.log(`Message received: ${msg.content}`);
-      },
-      {
-        noAck: true, //true to close connection when a message has been received - false to keep it running
-      }
-    );
-  });
-});
+// ...
+const GENDER_API = 'https://api.genderize.io';
+
+const URL = 'amqp://localhost';
+const QUEUE = 'myqueue';
+// const QUEUE_B = 'testcoding';
+
+async function loadPersons(event) {
+	const data = fs.readFileSync('src/assets/persons.json');
+	const json = JSON.parse(data);
+
+	return json[event.content] || { error: `Name (${event.content}) not found` };
+}
+
+async function loadMailTemplate(event) {
+	if (event.error) return event;
+
+	let mailTemplate = await fs.readFileSync('src/assets/mail.txt', 'utf-8');
+
+	const title = event.gender == 'male' ? 'mr.' : event.gender == 'female' ? 'ms.' : 'mx.';
+
+	mailTemplate = mailTemplate.replace('XX', title);
+	mailTemplate = mailTemplate.replace('NN', event.name);
+
+	return { email: event.email, mail: mailTemplate };
+}
+
+async function getGender(event) {
+	if (event.error) return event;
+
+	const response = await fetch(`${GENDER_API}/?name=${event.name}`);
+	const json = await response.json();
+
+	return { ...json, email: event.email };
+}
+
+async function processHandler(msg) {
+	// route
+	const route = new eip.Route('myRoute', { route: { retryLimit: 3, retryDelay: 1000 }, isErrorRoute: false }, []);
+
+	// process / filter
+	route.process(loadPersons);
+	route.process(getGender);
+	route.process(loadMailTemplate).info();
+
+	// event injection
+	route.inject(msg);
+}
+
+// ...
+(async () => {
+	// Step 1: Create connection & channel in RabbitMQ.
+	const connection = await amqp.connect(URL);
+	const channel = await connection.createChannel();
+
+	// Step 2: Assert Queue.
+	channel.assertQueue(QUEUE);
+
+	// Step 4:
+	channel.consume(QUEUE, processHandler, { noAck: true });
+})();
--- src/sender.js
+++ src/sender.js
@@ -1,20 +1,21 @@
-const amqp = require("amqplib/callback_api");
+const amqp = require('amqplib/channel_api');
 
-// Step 1: Create Connection
-amqp.connect("amqp://localhost", (connError, connection) => {
-  if (connError) {
-    throw connError;
-  }
-  // Step 2: Create Channel
-  connection.createChannel((channelError, channel) => {
-    if (channelError) {
-      throw channelError;
-    }
-    // Step 3: Assert Queue
-    const QUEUE = "codingtest";
-    channel.assertQueue(QUEUE);
-    // Step 4: Send message to queue
-    channel.sendToQueue(QUEUE, Buffer.from("hello from Pernille"));
-    console.log(`Message send ${QUEUE}`);
-  });
-});
+const URL = 'amqp://localhost';
+const QUEUE = 'myqueue';
+
+let payload = 'rasmus';
+
+(async () => {
+	// Step 1: Create connection & channel in RabbitMQ.
+	const connection = await amqp.connect(URL);
+	const channel = await connection.createChannel();
+
+	// Step 2: Assert Queue.
+	channel.assertQueue(QUEUE);
+
+	// Step 3: Send message to queue.
+	channel.sendToQueue(QUEUE, Buffer.from(payload));
+
+	// Step 4: Logs information.
+	console.log(`Message send ${QUEUE}`);
+})();
